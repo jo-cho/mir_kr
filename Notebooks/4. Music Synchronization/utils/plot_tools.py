5b1ec2f5d2625066288877a81ab74b0ac12f1f59
JH Cho <jhcho1016@naver.com>
2023-01-02 19:41:13 +0900

    update

--- Notebooks/4. Music Synchronization/utils/plot_tools.py
+++ Notebooks/4. Music Synchronization/utils/plot_tools.py
@@ -666,3 +666,32 @@ def plot_segments_overlay(*args, **kwargs):
     fig, ax = plot_segments(*args, **kwargs)
 
     return fig, 
+
+
+#--------------------
+
+def plot_matrix_with_points(C, P=np.empty((0, 2)), color='r', marker='o', linestyle='', **kwargs):
+    """Compute the cost matrix of two feature sequences
+    Args:
+        C: Matrix to be plotted
+        P: List of index pairs, to be visualized on the matrix (Default value = np.empty((0, 2)))
+        color: The color of the line plot (Default value = 'r').
+            See https://matplotlib.org/users/colors.html
+        marker: The marker of the line plot (Default value = 'o').
+            See https://matplotlib.org/3.1.0/api/markers_api.html
+        linestyle: The line-style of the line plot (Default value = '').
+            See https://matplotlib.org/gallery/lines_bars_and_markers/line_styles_reference.html
+        **kwargs: keyword arguments for :func:`libfmp.b.b_plot.plot_matrix`
+    Returns:
+        fig: Handle for figure
+        im: Handle for imshow
+        line: handle for line plot
+    """
+
+    fig, ax, im = plot_matrix(C, **kwargs)
+    line = ax[0].plot(P[:, 1], P[:, 0], marker=marker, color=color, linestyle=linestyle)
+
+    if fig is not None:
+        plt.tight_layout()
+
+    return fig, im, line
