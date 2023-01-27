44e4e700ad46176bc41ff7408bc3d546dffcd6fa
jojothepizza <52461409+jo-cho@users.noreply.github.com>
2023-01-27 23:57:26 +0900

    Update plot_tools.py

--- Notebooks/9. Musically Informed Audio Decomposition/utils/plot_tools.py
+++ Notebooks/9. Musically Informed Audio Decomposition/utils/plot_tools.py
@@ -11,6 +11,8 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.gridspec as gridspec
 import matplotlib.patches as mpatch
+import IPython.display as ipd
+import pandas as pd
 
 
 FMP_COLORMAPS = {
@@ -695,3 +697,34 @@ def plot_matrix_with_points(C, P=np.empty((0, 2)), color='r', marker='o', linest
         plt.tight_layout()
 
     return fig, im, line
+
+
+def audio_player_list(signals, rates, width=270, height=40, columns=None, column_align='center'):
+    """Generate a list of HTML audio players tags for a given list of audio signals.
+    Notebook: B/B_PythonAudio.ipynb
+    Args:
+        signals (list): List of audio signals
+        rates (list): List of sample rates
+        width (int): Width of player (either number or list) (Default value = 270)
+        height (int): Height of player (either number or list) (Default value = 40)
+        columns (list): Column headings (Default value = None)
+        column_align (str): Left, center, right (Default value = 'center')
+    """
+    pd.set_option('display.max_colwidth', None)
+
+    if isinstance(width, int):
+        width = [width] * len(signals)
+    if isinstance(height, int):
+        height = [height] * len(signals)
+
+    audio_list = []
+    for cur_x, cur_Fs, cur_width, cur_height in zip(signals, rates, width, height):
+        audio_html = ipd.Audio(data=cur_x, rate=cur_Fs)._repr_html_()
+        audio_html = audio_html.replace('\n', '').strip()
+        audio_html = audio_html.replace('<audio ', f'<audio style="width: {cur_width}px; height: {cur_height}px" ')
+        audio_list.append([audio_html])
+
+    df = pd.DataFrame(audio_list, index=columns).T
+    table_html = df.to_html(escape=False, index=False, header=bool(columns))
+    table_html = table_html.replace('<th>', f'<th style="text-align: {column_align}">')
+    ipd.display(ipd.HTML(table_html))
